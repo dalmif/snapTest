c1b4870d6058f0e2f2ed7d42f9f4bb78238a3f8a
Mohammad Fallah <barnamenevisfarsi@yahoo.com>
2018-12-26 10:24:12 +0330

    Initial Commit

---
+++ snapTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snapTest
+//
+//  Created by Mohammad Fallah on 10/5/1397 AP.
+//  Copyright © 1397 Mohammad Fallah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
